26e3991f5d749464b28d17c09c8c6d589494e544
0097eo <emmanuelokello294@gmail.com>
2025-02-11 21:41:28 +0300

    Add custom UserAdmin for enhanced user management in admin panel

--- shop/apps/accounts/admin.py
+++ shop/apps/accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'user_type', 'is_staff', 'is_active', 'is_verified')
+    list_filter = ('is_staff', 'is_active', 'user_type', 'is_verified')
+    search_fields = ('email', 'first_name', 'last_name', 'phone_number')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'phone_number')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_admin', 'is_verified', 'user_type')}),
+        ('Security', {'fields': ('verification_code', 'password_reset_token', 'password_reset_token_created')}),
+        
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'phone_number', 'user_type', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
