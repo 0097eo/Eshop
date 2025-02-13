b3f889efac479cc5eaa61456a49ad01c958d3fe3
0097eo <emmanuelokello294@gmail.com>
2025-02-13 18:29:40 +0300

    Refactor cart app configuration and update URL routing to include cart endpoints

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('apps.accounts.urls')),
-    path('products/', include('apps.products.urls') )
+    path('products/', include('apps.products.urls') ),
+    path('cart/', include('apps.cart.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
