f79edafa4826864714e75613442d7df445c21ce3
0097eo <emmanuelokello294@gmail.com>
2025-04-15 14:49:00 +0300

    Update ALLOWED_HOSTS default value in settings.py for production deployment

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'eshop-zkkr.onrender.com').split(',')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
