e4dedf14551f0d9a6aa241001367ad9bc7835b44
Emmanuel Otieno <41443081+0097eo@users.noreply.github.com>
2026-02-04 11:12:49 +0300

    Remove superuser creation from build.sh
    Removed superuser creation step from build script.

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
+#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 # Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
+python manage.py collectstatic --no-input
+
 # Run migrations
 python manage.py migrate
-
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
